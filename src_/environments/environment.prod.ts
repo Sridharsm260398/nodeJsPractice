135dee7e18cce0fdcd4c15635d50bc1b0d5aa9cf
Sridharsm260398 <sridhasmwork@gmail.com>
2024-08-17 10:51:30 +0530

    Revert "new folder"
    This reverts commit 1173ba677b99d1a05e17cf9ed2d8883749e0cecb.

--- src_/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  //apiUrlv1: 'https://webapp-rest-api-postgres-v1-sridhar.onrender.com/',
-  apiUrl: 'https://sridhars-webapp-restapi-postgres-v2.onrender.com/'
-};
--- src_/main.ts
+++
@@ -1,8 +0,0 @@
-
-
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/root/app.config';
-import { AppComponent } from './app/root/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
